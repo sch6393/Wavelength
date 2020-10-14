5b78cc7c08b884d8376037d719a2912b8c49e343
sch6393 <sch6393@gmail.com>
2020-10-14 15:54:25 +0900

    Add FullscreenActivity

--- app/src/main/java/sch6393/app/wavelength/ui/info/InfoFragment.kt
+++ app/src/main/java/sch6393/app/wavelength/ui/info/InfoFragment.kt
@@ -1,6 +1,12 @@
 package sch6393.app.wavelength.ui.info
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,22 +16,51 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import sch6393.app.wavelength.R
 
+
 class InfoFragment : Fragment() {
 
+    private val start = 77
+    private val end = 80
     private lateinit var infoViewModel: InfoViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         infoViewModel =
                 ViewModelProvider(this).get(InfoViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_info, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
+        val textView: TextView = root.findViewById(R.id.text_info)
         infoViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val textView2: TextView = root.findViewById(R.id.text_info2)
+        infoViewModel.text2.observe(viewLifecycleOwner, Observer {
+            val spannableStringBuilder = SpannableStringBuilder(it)
+            spannableStringBuilder.setSpan(
+                ForegroundColorSpan(Color.BLUE),
+                start,
+                end,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            spannableStringBuilder.setSpan(
+                StyleSpan(Typeface.BOLD),
+                start,
+                end,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            textView2.text = spannableStringBuilder
+        })
+
+        val textView3: TextView = root.findViewById(R.id.text_info3)
+        infoViewModel.text3.observe(viewLifecycleOwner, Observer {
+            textView3.text = it
+            textView3.setTextColor(Color.RED)
+        })
+
         return root
     }
 }
--- app/src/main/java/sch6393/app/wavelength/ui/info/InfoViewModel.kt
+++ app/src/main/java/sch6393/app/wavelength/ui/info/InfoViewModel.kt
@@ -7,7 +7,19 @@ import androidx.lifecycle.ViewModel
 class InfoViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is info Fragment"
+        value = "Optically Improved Mitochondrial Function Redeems Aged Human Visual Decline" +
+                "\nhttps://academic.oup.com/biomedgerontology/article/75/9/e49/5863431"
     }
     val text: LiveData<String> = _text
+
+    private val _text2 = MutableLiveData<String>().apply {
+        value = "붉은 빛을 내는 긴 파장(670nm)을 눈에 쐬면 원추세포와 간상세포의 활성화에 도움을 줄 수 있어요." +
+                "\n평소 명암 구분과 색 구분, 특히 파란색 구분이 잘 안된다면 한 번 붉은 빛을 눈에 쐬보세요."
+    }
+    val text2: LiveData<String> = _text2
+
+    private val _text3 = MutableLiveData<String>().apply {
+        value = "당연하지만 의료계 검증이 나오기 전까지는 조심하세요."
+    }
+    val text3: LiveData<String> = _text3
 }
